25dda92224834eea7442819fbab7b24732ba66d0
hujie <hujie@17luma.com>
2022-06-23 09:47:50 +0800

    首次提交

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,27 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
   extends: [
     'plugin:vue/vue3-essential',
     '@vue/airbnb',
-    '@vue/typescript/recommended',
+    '@vue/typescript/recommended'
   ],
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2020
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
+    'comma-dangle': [
+      'error', {
+        arrays: 'never',
+        objects: 'never',
+        imports: 'never',
+        exports: 'never',
+        functions: 'never'
+      }
+    ]
+  }
 };
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -38,8 +38,8 @@ import { Options, Vue } from 'vue-class-component';
 
 @Options({
   props: {
-    msg: String,
-  },
+    msg: String
+  }
 })
 export default class HelloWorld extends Vue {
   msg!: string
